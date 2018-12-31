393c5154c0ecfa06de2b5e2d8721d1e02df3e2c1
tiget <tigetxd@gmail.com>
2018-12-31 17:56:29 +0300

    Доделать изменение фрагмента при нажатии на forward/rewind кнопки(Песня меняется, информация нет), SongInfoFragment: кнопки like, hide(есть в коде), remove(есть в коде)

--- app/src/main/java/com/example/tiget/musicplayer/ui/SongInfoFragment.java
+++ app/src/main/java/com/example/tiget/musicplayer/ui/SongInfoFragment.java
@@ -3,6 +3,7 @@ package com.example.tiget.musicplayer.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.tiget.musicplayer.R;
+import com.example.tiget.musicplayer.ui.UserLibrary.UserLibDatabase;
+import com.example.tiget.musicplayer.ui.UserLibrary.UserLibSong;
 
 public class SongInfoFragment extends Fragment {
 
@@ -26,6 +30,12 @@ public class SongInfoFragment extends Fragment {
     private static String mSongName;
     private static int mResId;
 
+    private RelativeLayout mLikeSong;
+    private RelativeLayout mHideSong;
+    private RelativeLayout mAddSong;
+    private RelativeLayout mRemoveSong;
+
+    private UserLibDatabase mDatabase;
 
 
     public static SongInfoFragment newInstance(String SongUri, String AuthorName, String SongName, int ResId) {
@@ -61,24 +71,23 @@ public class SongInfoFragment extends Fragment {
         AuthorNameTV = view.findViewById(R.id.AuthorName);
         CloseArea = view.findViewById(R.id.closeArea);
 
+        mLikeSong = view.findViewById(R.id.likeSong);
+        mHideSong = view.findViewById(R.id.hideSong);
+        mAddSong = view.findViewById(R.id.addSong);
+        mRemoveSong = view.findViewById(R.id.removeSong);
+
         SongPreviewIV.setImageResource(mResId);
         SongNameTV.setText(mSongName);
         AuthorNameTV.setText(mAuthorName);
 
-/*
-        CloseArea.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, new MusicFragment()).commit();
-                if(MiniMediaPlayerFragment.mPauseButton != null) {
-                    t.showMiniMediaFragment(mSongUri, mAuthorName, mSongName, mResId, getActivity());
-                }
-
-            }
-        });
-        */
+        mDatabase = new UserLibDatabase(getActivity());
 
 
+
+/**
+ * Half-transparent area below the image. There must be the library fragment underneath it, but something went wrong.
+ * Being used to close info fragment on click.
+ */
         CloseArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -90,17 +99,20 @@ public class SongInfoFragment extends Fragment {
             }
         });
 
+
+
+        mAddSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mDatabase.alreadyExists(mSongUri)) {
+                    Snackbar.make(mAddSong, "Данная песня уже у вас в плейлисте", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    mDatabase.addSong(new UserLibSong(0, mAuthorName, mSongName, mSongUri, mResId));
+                    Snackbar.make(mAddSong, "Песня добавлена в ваш плейлист", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
-
-
-/*
-
-    if(mDatabase.alreadyExists(song.getSongUri())) {
-                    Snackbar.make(holder.itemView, "Данная песня уже у вас в плейлисте", Snackbar.LENGTH_SHORT).show();
-                } else {
-                    mDatabase.addSong(new UserLibSong(0, song.getAuthorName(), song.getSongName(), song.getSongUri(), song.getSongPreview()));
-                    //Snackbar.make(holder.itemView, "Песня добавлена в ваш плейлист", Snackbar.LENGTH_SHORT).show();
-                }
-
- */
